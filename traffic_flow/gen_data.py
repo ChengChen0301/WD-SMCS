79fe4ed9cad5fe698b85f6cf75392d1724cb6217
ChengChen0301 <70123387+ChengChen0301@users.noreply.github.com>
2023-06-11 16:28:10 +0800

    Update gen_data.py

--- traffic_flow/gen_data.py
+++ traffic_flow/gen_data.py
@@ -41,37 +41,9 @@ if __name__ == '__main__':
                 x1.append((sim.roads[j].vehicles[k].x, sim.roads[j].start[1], sim.roads[j].vehicles[k].v, sim.roads[j].vehicles[k].alpha))
                 noiseFlag = 1
                 u = random.gauss(0, 0.1)
-                # while noiseFlag == 1:
-                #     u = random.gauss(0, 0.5)
-                #     if sim.roads[j].start[0] <= sim.roads[j].vehicles[k].x + u <= sim.roads[j].end[0]:
-                #         noiseFlag = 0
-                #         break
-                #     if sim.roads[j].end[0] <= sim.roads[j].vehicles[k].x + u <= sim.roads[j].start[0]:
-                #         noiseFlag = 0
-                #         break
                 x2.append((sim.roads[j].vehicles[k].x + u, sim.roads[j].start[1], sim.roads[j].vehicles[k].v, sim.roads[j].vehicles[k].alpha))
-        np.savetxt('exact/round5/truth' + str(i+1) + '.txt', x1)
-        # np.savetxt('observation/round5/obsv' + str(i+1) + '.txt', x2)
-
-
-
-
-
-
-# for t in range(50):
-#
-#     x0 = np.loadtxt('samples5/' + str(t+1) + '.txt').reshape(-1, 3)[:, 0:2]
-#     w0 = np.ones((len(x0),)) / len(x0)
-#     x1 = np.loadtxt('samples4/' + str(t+1) + '.txt').reshape(-1, 3)[:, 0:2]
-#     w1 = np.ones((len(x1),)) / len(x1)
-#     M = np.sqrt(ot.dist(x0, x1))
-#     emd = ot.emd2(w0, w1, M, numItermax=1e7)
-#     print(emd)
-
-
-
-
-
+        np.savetxt('observation/vehicle_small_noise/truth' + str(i+1) + '.txt', x1)
+        np.savetxt('observation/vehicle_large_noise/obsv' + str(i+1) + '.txt', x2)
 
 
 
